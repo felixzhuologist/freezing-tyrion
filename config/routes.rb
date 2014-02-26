10e8ae25d4bc92365ef2ff06c1b79b88f89566fd
Felix <felix.czhu@gmail.com>
2014-02-25 22:28:52 -0500

    Finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FelixzHu::Application.routes.draw do
+  root 'static_pages#home'
+  match '/shopify',    to: 'static_pages#shopify',    via: 'get' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
